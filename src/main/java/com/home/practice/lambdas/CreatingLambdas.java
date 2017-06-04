5783004cf13bea74c68a8b54142ef2516934be42
Baba <Baba@192.168.1.8>
2017-06-04 17:26:14 +0530

    Removing unused code

--- src/main/java/com/home/practice/lambdas/CreatingLambdas.java
+++ src/main/java/com/home/practice/lambdas/CreatingLambdas.java
@@ -1,4 +1,4 @@
-package code.practice;
+package com.home.practice.lambdas;
 
 public class CreatingLambdas {
 	
@@ -12,24 +12,12 @@ public class CreatingLambdas {
 		System.out.println(result);
 	}
 	
-	// Target Types
-	public void methodAcceptingTestInterface(TestInterface testInterface) {
-		System.out.println(testInterface.sayHello("Nash"));
-	}
-	
-	// Capturing Lambda
-	public void methodAcceptingNull(String name) {
-		TestInterface ti = (greet) -> name.trim().isEmpty() ? "Hi " : "Hello " + name;
-		System.out.println(ti.sayHello(name));
-	}
-	
 	public static void main(String[] args) {
 		
 		new CreatingLambdas().testGreeting("nahusha", new Greeting() {
 			
 			@Override
 			public String sayHello(String name) {
-				// TODO Auto-generated method stub
 				 return "Hello " + name;
 			}
 		});
@@ -38,14 +26,6 @@ public class CreatingLambdas {
 		new CreatingLambdas().testGreeting("nahusha", (String s) -> "Hello " + s);
 		
 		new CreatingLambdas().testGreeting("", (String s) -> s.isEmpty() ? "Did you miss something? " : "Hello "+ s);
-		
-		TestInterface tI = (s) -> {
-			return "Hi, " + s;
-		};
-		
-		new CreatingLambdas().methodAcceptingTestInterface(tI);
-		
-		new CreatingLambdas().methodAcceptingNull("  ");
 		
 	}
 }
